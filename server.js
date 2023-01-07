203019b93168127853d57d4c0b01096525ffbef8
Martin Lin <yam22975918@gmail.com>
2023-01-07 15:20:32 +0800

    Make changes for Deployment

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ const courseRoute = require("./routes").course;
 const passport = require("passport");
 require("./config/passport")(passport);
 const cors = require("cors");
+const path = require("path");
+const port = process.env.PORT || 8080; // process.env.PORT 是 Heroku 自動動態設定
 
 mongoose.set('strictQuery', true);
 // 連接 mongoDB
 mongoose
-    .connect("mongodb://127.0.0.1:27017/mernDB")
+    .connect(process.env.MONGODB_CONNECTION)
     .then(() => {
         console.log("Connecting to mongodb...");
     })
@@ -24,6 +26,7 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 // 若接收到任何 route 為 /api/user 之路徑，則去連結至 auth
 app.use("/api/user", authRoute);
@@ -38,7 +41,16 @@ app.use(
 // 只有登入系統的人，才能夠去新增課程或是註冊課程
 // jwt
 
+if(
+    process.env.NODE_ENV === "production" ||
+    process.env.NODE_ENV === "staging"
+) {
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+}
+
 // need to judge port 3000(because 3000 is react default port)
-app.listen(8080, () => {
+app.listen(port, () => {
     console.log("後端伺服器聆聽在 port 8080");
 });
